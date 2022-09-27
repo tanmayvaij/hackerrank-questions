56c1568d061fe892d27f346e4a073ea99912fd98
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-09-27 17:05:37 +0530

    warmup questions completed

---
+++ warmup/question_010.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+string timeConversion(string s) {
+
+    string military_time= "";
+    
+    if ( s.substr(0, 2) ==  "12" && s.substr(8, 2) == "AM" ) {
+        military_time.append("00");
+        military_time.append(s.substr(2, 6));
+    }
+    else if ( s.substr(8, 2) == "AM" ) {
+        military_time.append(s.substr(0, 8));
+
+    }
+    else if ( s.substr(0, 2) == "12" && s.substr(8, 2) == "PM" ) {
+        military_time.append(s.substr(0, 8));
+    }
+    else if ( s.substr(8, 2) == "PM" ) {
+        int n = stoi(s.substr(0, 2));
+        military_time.append(to_string(n+12));
+        military_time.append(s.substr(2, 6));
+    }
+
+    return military_time;
+
+}
+
+int main () {
+
+    cout << timeConversion("05:00:12PM") << endl;
+
+    return 0;
+
+}
